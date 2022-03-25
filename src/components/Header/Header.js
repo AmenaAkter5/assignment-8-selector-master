523cb019ab6b5d1d49797b8a832cddf2fe2d2a23
unknown <latapata1952@gmail.com>
2022-03-25 23:40:03 +0600

    header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <nav className="header">
+            <h1>Selector-Master</h1>
+        </nav>
+    );
+};
+
+export default Header;
