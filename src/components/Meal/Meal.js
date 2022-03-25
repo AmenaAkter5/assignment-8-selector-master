f467d7eb28a602ea881a873a5064f23547b1a4c5
unknown <latapata1952@gmail.com>
2022-03-26 01:04:42 +0600

    data display in UI

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Meal.css';
 
-const Meal = () => {
+const Meal = (props) => {
+
+    const { addToCart, meal } = props
+    const { name, img, price } = meal;
+
     return (
         <div>
-            <h1>Meal { }</h1>
+            <div className="meal">
+                <img src={img} alt="" />
+                <h3 className="meal-name">{name}</h3>
+                <p className="meal-price">Price: <b>${price}</b></p>
+                <button onClick={addToCart} className="card-btn">
+                    <p>Add to Cart</p>
+                </button>
+            </div>
         </div>
     );
 };
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,25 +1,39 @@
 import React, { useEffect, useState } from 'react';
+import Meal from '../Meal/Meal';
 import './Meals.css';
 
 const Meals = () => {
 
+    // data load state
     const [meals, setMeals] = useState([]);
 
+    // data fetch
     useEffect(() => {
         fetch('meals.json')
             .then(res => res.json())
             .then(data => setMeals(data))
     }, []);
 
+    // btn event handler
+    const addToCart = () => {
+        console.log('added');
+    }
+
     return (
-        <div>
-            <h1>This is Meal {meals.length}</h1>
-            {
-                meals.map(meal => console.log(meal))
-            }
-            {
-                // meals.map(meal => console.log(meal))
-            }
+        <div className="meals">
+            <div className="meals-container">
+                {
+                    meals.map(meal => <Meal
+                        key={meal.id}
+                        meal={meal}
+                        addToCart={addToCart}
+                    ></Meal>)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>This is cart</h3>
+                <p>selected items { }</p>
+            </div>
         </div>
     );
 };
