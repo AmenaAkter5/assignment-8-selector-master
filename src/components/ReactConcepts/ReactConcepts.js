0b3ee56330381ec93aee9ebeb48102cf6e312c90
unknown <latapata1952@gmail.com>
2022-03-26 18:56:26 +0600

    submit

--- src/components/ReactConcepts/ReactConcepts.js
+++ src/components/ReactConcepts/ReactConcepts.js
@@ -7,14 +7,14 @@ const ReactConcepts = () => {
             <h1 className="blog-title">React Concepts</h1>
             <div className="blog-container">
                 <article className="blog">
-                    <h2>How React Works</h2>
+                    <h1>How React Works</h1>
                     <p>React is an open-source JavaScript package that is used to create single-page applications user interfaces.
                         It is simple, declarative and component based JavaScript library.
                     </p>
                     <p>In React, instead of using regular JavaScript for templating, it uses JSX. JSX is a simple JavaScript that allows HTML syntax in JavaScript. HTML syntax is processed into JavaScript objcet by Babel transpiler. Then it renders that object on React DOM. According to change on react DOM it re render the changed part on actual DOM. So it has good performance result because it done its job at shorter time. React done this complete process by diff algorithom.</p>
                 </article>
                 <article className="blog">
-                    <h2>Props Vs State</h2>
+                    <h1>Props Vs State</h1>
                     <p>Props and State can be used to pass data between components.
                         So, show us how to send data via props and state, and what the differences are between the two.
                     </p>
